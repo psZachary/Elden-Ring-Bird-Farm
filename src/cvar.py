8d09576d44a0c8463aeafe66fb9c7dc4595c01ff
Fozzila <47261750+Fozzila@users.noreply.github.com>
2022-04-04 20:04:36 -0400

    Added rest at grace interval
    - Added rest at grace interval
    - Added rest at grace counter

--- src/cvar.py
+++ src/cvar.py
@@ -4,7 +4,9 @@ import configparser
 config = configparser.ConfigParser()
 config.read(os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + "\\config.ini")
 
-load_time = config['DEFAULT']['load_time']
-toggle_farm_key = config['DEFAULT']['toggle_farm_key']
-exit_program_key = config['DEFAULT']['exit_program_key']
+load_time = config['SETTINGS']['load_time']
+toggle_farm_key = config['SETTINGS']['toggle_farm_key']
+exit_program_key = config['SETTINGS']['exit_program_key']
+rest_at_grace_interval = config['SETTINGS']['rest_at_grace_interval']
+rest_at_grace_counter = 0
 farm_running = False
